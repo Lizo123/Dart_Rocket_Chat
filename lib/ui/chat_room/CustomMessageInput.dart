363d1e342d09cb89942047d7e995bad5ee2e7a05
FatmaMM <fabdelmuhsen@egypt.itgsolutions.com>
2020-09-16 15:01:24 +0200

    ui updates for attachments

--- lib/ui/chat_room/CustomMessageInput.dart
+++ lib/ui/chat_room/CustomMessageInput.dart
@@ -125,11 +125,8 @@ class CustomMessageInputState extends State<CustomMessageInput> {
 
   Future<CameraDescription> getCamera() async {
     WidgetsFlutterBinding.ensureInitialized();
-    // Obtain a list of the available cameras on the device.
     final cameras = await availableCameras();
-    // Get a specific camera from the list of available cameras.
     final firstCamera = cameras.first;
-
     return firstCamera;
   }
 
